eb484cc9b622b90fc0d0524cd4604df0573018e3
theblindbandet <theblindbandet@users.noreply.github.com>
2018-04-26 20:16:22 -0400

    Add example OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/springtraining/examples/ExampleAutonomous.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.springtraining.examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Disabled
+@Autonomous(name = "ExampleAutonomous", group = "Example")
+public class ExampleAutonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Init
+        waitForStart();
+        // Run once
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/springtraining/examples/ExampleTeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.springtraining.examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp(name = "ExampleTeleOp", group = "Example")
+public class ExampleTeleOp extends OpMode {
+    @Override
+    public void init() {
+        // Init
+    }
+
+    @Override
+    public void loop() {
+        // Loop
+    }
+}
